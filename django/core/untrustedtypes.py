4c5d4ed91a9c10d227455d38a8174f29f9143d0b
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-01-19 18:25:39 -0500

    Streamline implementation to remove the need to pass a base class along by using __mro__.

--- django/core/untrustedtypes.py
+++ django/core/untrustedtypes.py
@@ -4,14 +4,11 @@ Untrusted classes
 from collections import UserString
 
 
-def add_synthesis(func, *, cls, base):
+def add_synthesis(func):
     """A function decorator that decorates func.
-    cls is expected to be of an UntrustedX type.
-    For example, cls might be UntrustedInt. base
-    (if not None) is expected to be the base case
-    from which cls is derived. For example, base
-    might be int. This function handled various
-    types of funcs in cls."""
+    This decorator makes an UntrustedX class inherit
+    all its base (non-untrusted) class functions
+    and return Untrusted values."""
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
         if res is NotImplemented:
@@ -24,32 +21,44 @@ def add_synthesis(func, *, cls, base):
         for key, value in kwargs.items():
             if issubclass(type(value), UntrustedMixin):
                 synthesized = synthesized or value.synthesized
-        # TODO: This might need to be modified later!
-        if type(res) == base:
-            # Only the result of the base type is casted to UntrustedX type
-            return cls(res, synthesized=synthesized)
-        elif type(res) == cls:
-            # Sometimes inheritances use self.__class__() to create UntrustedX
-            # type in base class but synthesized flag might not be set correctly
+
+        if isinstance(res, UntrustedMixin):
+            # If res is already an Untrusted type (e.g., in UserStr inheritance uses
+            # self.__class__() to create UntrustedStr instead of UserStr or str
+            # class objects, but synthesized flag might not be set correctly!
             res.synthesized = synthesized
             return res
+        # boolean value is always bool (bool is not extensible)
+        # bool is a subclass of int, so we must check this first
+        elif isinstance(res, bool):
+            return res
+        elif isinstance(res, int):
+            return UntrustedInt(res, synthesized=synthesized)
+        elif isinstance(res, float):
+            return UntrustedFloat(res, synthesized=synthesized)
+        elif isinstance(res, str) or isinstance(res, UserString):
+            return UntrustedStr(res, synthesized=synthesized)
+        #####################################################
+        # TODO: Add more casting here for new untrusted types
+        #####################################################
+        # TODO: We may consider a generic Untrusted type,
+        #  instead of returning a trusted value.
         else:
             return res
+
     return wrapper
 
 
-def add_synthesis_to_func(cls, base):
+def add_synthesis_to_func(cls):
     """A class decorator that decorates all functions in cls
     so that they are synthesis-aware. If cls is a subclass of
-    a "base" class (passed by as a parameter by UntrustedX
-    subclasses), then we want functions in base class to be
-    synthesis-aware as well. "base" can be None, in which case
-    we would only decorate "cls" functions.
+    some base classes, then we want functions in base class to be
+    synthesis-aware as well.
 
-    Important note: Any cls function to be decorated must
+    Important note: Any cls (UntrustedX) function to be decorated must
     start with either "synthesis_" or "_synthesis_" (for protected
     methods), while base class functions have no such restriction
-    (since it is possible that developers have no control over the
+    (since it is likely that developers have no control over the
     base class). Using this convention, developers can prevent
     base class functions from being decorated by overriding the
     function (and just calling base class function). If, for example,
@@ -59,48 +68,57 @@ def add_synthesis_to_func(cls, base):
     call the helper function in the dunder function. As such,
     _synthesize__dunder__ will be decorated (and therefore the
     calling dunder function)."""
+    # set of function names already been decorated/inspected
+    handled_funcs = set()
     # First handle all functions in cls class
     for key, value in cls.__dict__.items():
         # Only callable functions are decorated
         if not callable(value):
             continue
+        # All callable functions are inspected in cls
+        handled_funcs.add(key)
         if key.startswith("synthesis_") or key.startswith("_synthesis_"):
-            setattr(cls, key, add_synthesis(value, cls=cls, base=base))
-    # Handle base class functions if exists. Base class is
+            setattr(cls, key, add_synthesis(value))
+    # Handle base class functions if exists. Base classes are
     # unlikely to follow our synthesis naming convention.
-    # However, some dunder methods (as in excluded_funcs)
-    # clearly should *not* be decorated. Some can be more subtle:
+    # However, some dunder methods clearly should *not* be
+    # decorated, we will add them in handled_funcs.
+    # Some can be more subtle:
     # * __int__: This dunder method is called during int().
     #            If int() is called, we should not decorate it
     #            but actually returns an int typed value.
-    excluded_funcs = ['__dict__', '__module__', '__doc__', '__repr__',
-                      '__getattribute__', '__str__', '__new__', '__format__',
-                      '__int__']
-    if base is not None:
+    handled_funcs.update({'__dict__', '__module__', '__doc__', '__repr__',
+                          '__getattribute__', '__str__', '__new__', '__format__',
+                          '__int__'})
+    # __mro__ defines the list of *ordered* base classes (the first being cls and
+    # the second being UntrustedMixin; UntrustedMixin should *not* be decorated)
+    for base in cls.__mro__[2:]:
         for key, value in base.__dict__.items():
-            # Only callable functions that are not handled by cls
-            if key in cls.__dict__ or not callable(value):
+            # Only callable functions that are not handled by previous classes
+            if not callable(value) or key in handled_funcs:
                 continue
-            # Some dunder methods clearly should not be decorated
-            if key in excluded_funcs:
-                continue
+            # All callable functions are inspected in the current base class
+            handled_funcs.add(key)
             # Delegate add_synthesis() to handle other cases
             # since there is not much convention we can specify.
             # Note that it is possible add_synthesis() can just
             # return the same function (value) with no changes.
-            setattr(cls, key, add_synthesis(value, cls=cls, base=base))
+            setattr(cls, key, add_synthesis(value))
     return cls
 
 
 class UntrustedMixin(object):
-    """A Mixin class for adding the Untrusted feature to other classes."""
+    """A Mixin class for adding the Untrusted feature to other classes.
+
+    Important note: for __init_subclass__'s add_synthesis_to_func() to work
+    correct, UntrustedMixin must used as the *first* parent class in a subclass."""
     def __init__(self, synthesized=False, *args, **kwargs):
         """A synthesized flag to id if a value is synthesized."""
         # Forwards all unused arguments to other base classes down the MRO line.
         self._synthesized = synthesized
         super().__init__(*args, **kwargs)
 
-    def __init_subclass__(cls, *, base=None, **kwargs):
+    def __init_subclass__(cls, **kwargs):
         """Whenever a class inherits from this, this function is called on that class,
         so that we can change the behavior of subclasses. This is closely related to
         class decorators, but where class decorators only affect the specific class
@@ -108,14 +126,9 @@ class UntrustedMixin(object):
         the class defining the method.
 
         Here we use both __init_subclass__ and class decorator, so that a subclass
-        of UntrustedMixin and its subclasses can be decorated.
-
-        TODO: multiple-inheritance might not work correctly with "base", see:
-        https://stackoverflow.com/questions/55183288/inheriting-init-subclass-parameters
-        However, inheriting from UntrustedX class is discouraged. In this case,
-        we can also simply directly use class decorators instead of __init_subclass__()."""
+        of UntrustedMixin and its subclasses can be decorated."""
         super().__init_subclass__(**kwargs)
-        add_synthesis_to_func(cls, base)
+        add_synthesis_to_func(cls)
 
     @property
     def synthesized(self):
@@ -126,10 +139,9 @@ class UntrustedMixin(object):
         self._synthesized = synthesized
 
 
-class UntrustedInt(UntrustedMixin, int, base=int):
+class UntrustedInt(UntrustedMixin, int):
     """Subclass Python builtin int class and Untrusted Mixin.
-    Note that synthesized is a keyed parameter. The "base"
-    key is used as the argument to UntrustedMixin's __init_subclass__."""
+    Note that synthesized is a keyed parameter."""
     def __new__(cls, x, *args, synthesized=False, **kwargs):
         self = super().__new__(cls, x, *args, **kwargs)
         return self
@@ -163,10 +175,9 @@ class UntrustedInt(UntrustedMixin, int, base=int):
         return "{value}".format(value=super().__str__())
 
 
-class UntrustedFloat(UntrustedMixin, float, base=float):
+class UntrustedFloat(UntrustedMixin, float):
     """Subclass Python builtin float class and Untrusted Mixin.
-    Note that synthesized is a keyed parameter. The "base" key
-    is used as the argument to UntrustedMixin's __init_subclass__."""
+    Note that synthesized is a keyed parameter."""
     def __new__(cls, x, *args, synthesized=False, **kwargs):
         self = super().__new__(cls, x, *args, **kwargs)
         return self
@@ -175,12 +186,11 @@ class UntrustedFloat(UntrustedMixin, float, base=float):
         super().__init__(synthesized)
 
 
-class UntrustedStr(UntrustedMixin, UserString, base=UserString):
+class UntrustedStr(UntrustedMixin, UserString):
     """Subclass collections module's UserString to create
     a custom str class that behaves like Python's built-in
     str but allows further customization. Use UntrustedMixin
-    to add the untrusted feature for the class. The "base" key
-    is used as the argument to UntrustedMixin's __init_subclass__."""
+    to add the untrusted feature for the class."""
     def __init__(self, seq, *, synthesized=False):
         super().__init__(synthesized, seq)
 
@@ -212,14 +222,13 @@ class UntrustedStr(UntrustedMixin, UserString, base=UserString):
         return type(self).custom_hash(chars)
 
 
-if __name__ == "__main__":
-    # Test int
+def untrusted_int_test():
     base_int = int("A", base=16)
     int_literal = 5
     untrusted_int_1 = UntrustedInt(15)
     untrusted_int_2 = UntrustedInt("B", base=16)
     synthesized_int_1 = UntrustedInt(12, synthesized=True)
-    # Some expected test cases
+
     untrusted_int_3 = untrusted_int_1 + untrusted_int_2
     assert untrusted_int_3 == 26, "untrusted_int_3 should be 26, but it is {}.".format(untrusted_int_3)
     assert untrusted_int_3.synthesized is False, "untrusted_int_3 should not be synthesized."
@@ -275,7 +284,20 @@ if __name__ == "__main__":
     assert synthesized_int_5.synthesized is True, "synthesized_int_5 should be synthesized."
     assert type(synthesized_int_5) == type(synthesized_int_1), "synthesized_int_5 type is not UntrustedInt"
 
-    # Test float
+    synthesized_int_6 = int_literal * synthesized_int_1
+    assert synthesized_int_6 == 60, "synthesized_int_6 should be 60, but it is {}.".format(synthesized_int_6)
+    assert synthesized_int_6.synthesized is True, "synthesized_int_6 should be synthesized."
+    assert type(synthesized_int_6) == type(synthesized_int_1), "synthesized_int_6 type is not UntrustedInt"
+
+    synthesized_int_7 = synthesized_int_1 // int_literal
+    assert synthesized_int_7 == 2, "synthesized_int_7 should be 2, but it is {}.".format(synthesized_int_7)
+    assert synthesized_int_7.synthesized is True, "synthesized_int_7 should be synthesized."
+    assert type(synthesized_int_7) == type(synthesized_int_1), "synthesized_int_7 type is not UntrustedInt"
+
+
+def untrusted_float_test():
+    base_int = int("A", base=16)
+    untrusted_int_1 = UntrustedInt(15)
     base_float = float(1.5)
     float_literal = 5.5
     untrusted_float_1 = UntrustedFloat(10.5)
@@ -316,7 +338,8 @@ if __name__ == "__main__":
     assert untrusted_float_8.synthesized is False, "untrusted_float_8 should not be synthesized."
     assert type(untrusted_float_8) == type(untrusted_float_1), "untrusted_float_8 type is not UntrustedFloat"
 
-    # Test str
+
+def untrusted_str_test():
     base_str = str("Hello ")
     str_literal = "World!"
     untrusted_str = UntrustedStr("Untrusted World!")
@@ -363,3 +386,9 @@ if __name__ == "__main__":
                                                      " but it is {}.".format(synthesized_str_4)
     assert synthesized_str_4.synthesized is True, "synthesized_str_4 should be synthesized."
     assert type(synthesized_str_4) == type(untrusted_str), "synthesized_str_4 type is not UntrustedStr"
+
+
+if __name__ == "__main__":
+    untrusted_int_test()
+    untrusted_float_test()
+    untrusted_str_test()
