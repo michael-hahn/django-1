c6563bac856e7303d13f02750823bc16b18c3d02
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-01-28 13:01:44 -0500

    Change BST backend to support only key/value pairs since BST supports only key/value pairs now.

--- django/splice/backends/bst.py
+++ django/splice/backends/bst.py
@@ -1,4 +1,4 @@
-"""Binary search tree backend"""
+"""Binary search tree backend."""
 
 from django.splice.structures.bst import SynthesizableBST
 from django.splice.backends.base import BaseStruct
@@ -6,30 +6,31 @@ from django.splice.backends.base import BaseStruct
 
 class BaseBST(BaseStruct):
     def __init__(self):
-        """Create a new data structure instance."""
-        struct = SynthesizableBST()
-        super().__init__(struct)
+        """Create a new data structure backend for BST."""
+        super().__init__(SynthesizableBST())
 
     def save(self, data):
-        """data can either be a single value, a (key, value) tuple,
-        a list of values or a list of (key, value) tuples. If it is
-        a list (or values or tuples), insertion order follows list
-        order. For a single insertion, return False if the insertion
-        failed. For multi insertions, return False if at least one
-        insertion failed but insertions that succeeded not be rolled
-        back, so we may have a partial insertion."""
+        """
+        'data' can be a (key, value) tuple, or a list of
+        (key, value) tuples. If it is a list, insertion
+        order follows the list order. For a single insertion,
+        return False if the insertion failed. For multi insertions,
+        return False if at least one insertion failed but
+        insertions that succeeded prior to the failure won't be
+        rolled back, so we may have a partial insertion.
+        """
         if isinstance(data, tuple):
-            return self.struct.insert(val=data[1], key=data[0])
+            return self.struct.insert(data[0], data[1])
         elif isinstance(data, list):
             success = True
             for d in data:
                 if isinstance(d, tuple):
-                    success &= self.struct.insert(val=d[1], key=d[0])
+                    success &= self.struct.insert(d[0], d[1])
                 else:
-                    success &= self.struct.insert(val=d)
+                    raise ValueError("a (key, value) tuple is expected, but got {}".format(d))
             return success
         else:
-            return self.struct.insert(data)
+            raise ValueError("a (key, value) tuple is expected, but got {}".format(data))
 
     def get(self, key):
         return self.struct.get(key)
@@ -61,21 +62,3 @@ if __name__ == "__main__":
     print("Flattened key-value tree (before synthesis): {}".format(str(NameNumBST.objects)))
     print("Synthesizing root node success: {}".format(NameNumBST.objects.synthesize("Jake")))
     print("Flattened key-value tree (after synthesis): {}".format(str(NameNumBST.objects)))
-
-    class NameOnlyBST(Struct):
-        name = CharField()
-        struct = BaseBST()
-
-    bst = NameOnlyBST(name="Jake")
-    bst.save()
-    bst = NameOnlyBST(name="Blair")
-    bst.save()
-    bst = NameOnlyBST(name="Luke")
-    bst.save()
-    bst = NameOnlyBST(name="Andre")
-    bst.save()
-    bst = NameOnlyBST(name="Zack")
-    bst.save()
-    print("Flattened value-only tree (before synthesis): {}".format(str(NameOnlyBST.objects)))
-    print("Synthesizing root node success: {}".format(NameOnlyBST.objects.synthesize("Jake")))
-    print("Flattened value-only tree (after synthesis): {}".format(str(NameOnlyBST.objects)))
