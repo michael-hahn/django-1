7730e67ac935b36ae0edf0a3b9b49cb735fa4c7f
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-02-22 13:10:13 -0500

    Update __init__.py

--- django/splice/__init__.py
+++ django/splice/__init__.py
@@ -1,9 +1,4 @@
-"""Globally override function imports."""
-
 import sys
-
-from django.splice.untrustedtypes import is_synthesized, to_untrusted
-
 
 #  Override built-in functions by modifying Python's builtins module
 #  This type of module level override is risky since other modules
@@ -12,6 +7,12 @@ module = sys.modules['builtins']
 #  len(): we override this function because CPython performs a type
 #  validation on the return type of the builtin len() to ensure that
 #  the value is always of int type, even if we override __len__. To
-#  circumvent this check, we have to directly override len().
+#  circumvent this check, we have to directly override len(). The
+#  same applies to the built-in hash() as well.
 module.len = lambda obj: obj.__len__()
+module.hash = lambda obj: obj.__hash__()
 sys.modules['builtins'] = module
+
+
+if __name__ == "__main__":
+    pass
