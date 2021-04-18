c581f0d55638e3896c25dcb3c07e5739c5dc5bd4
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-04-18 14:53:52 -0400

    Splice's row-level tainting module.

--- django/splice/fields.py
+++ django/splice/fields.py
@@ -1,78 +1,76 @@
-from django.db import models
+"""
+If we use row-level tainting in DB, every field should inherit from SpliceFieldMixin
+to override the default contribute_to_class() method, which assigns a SpliceDescriptor
+to the field object, so that when the field is accessed from the model instance, the
+SpliceDescriptor is called to propagate taints/tags to the field object accordingly.
+This SpliceFieldMixin is useful for row-level tainting only.
+"""
+
 from django.splice.splice import SpliceMixin
 from django.splice.splicetypes import SpliceStr, SpliceInt
-from django.splice.identity import TaintSource
-from bitarray.util import int2ba, ba2int
+from django.splice.identity import to_bitarray
 
 
 # use Field's __class__.__qualname__ to find the corresponding Splice class
-type_dict = {'SpliceIntegerField': SpliceInt,
-             'SpliceCharField': SpliceStr}
+type_dict = {'IntegerField': SpliceInt,
+             'CharField': SpliceStr,
+             'SlugField': SpliceStr,}
 
 
-class SpliceCreator(object):
-    """Splice field descriptor object. It is like DeferredAttribute in Django source."""
+class SpliceDescriptor(object):
+    """
+    Splice field descriptor object. It is similar to DeferredAttribute
+    (db.models.query_utils) in Django. This allows model instance's taint
+    to "propagate" to its field. All non-relational, primitive-typed
+    field should use this descriptor to obtain taints. Django defines
+    its own descriptors for relational field, which Splice modifies for
+    taint propagation directly.
+    """
     def __init__(self, field):
         self.field = field
-        self.synthesized_field_name = "{}_synthesized".format(self.field.name)
-        self.taint_field_name = "{}_taint".format(self.field.name)
 
-    def __get__(self, obj, type=None):
-        if obj is None:
-            raise AttributeError('Can only be accessed via an instance')
+    def __get__(self, instance, type=None):
+        if instance is None:
+            raise AttributeError('taints can be laundered without instance')
 
-        data = obj.__dict__[self.field.name]
-        if data is None:
+        data = instance.__dict__
+        val = data[self.field.name]
+        if val is None:
+            print("SpliceDescriptor.__get__({}): value is None".format(self.field.name))
             return None
+        # A model instance might get its data after it is constructed from ModelForm
+        # (e.g., to perform validation). Since the data is not retrieved from DB, it
+        # is already a Splice-aware type; therefore, we don't need to cast it at all.
+        elif isinstance(val, SpliceMixin):
+            return val
         else:
             cls = type_dict.get(self.field.__class__.__qualname__)
             if cls is None:
                 raise AttributeError('Cannot find any registered Splice data type for {}'
                                      .format(self.field.__class__.__qualname__))
-            taints = getattr(obj, self.taint_field_name)
-            # Convert from integer into bitarray
-            ba = int2ba(taints, length=TaintSource.MAX_USERS, endian='big', signed=True)
-            return cls.splicify(data,
-                                trusted=False,
-                                synthesized=getattr(obj, self.synthesized_field_name),
-                                taints=ba)
+            # All models should inherit from SpliceDB to be Splice-aware
+            # All model instance inherited from SpliceDB has a "taints" column.
+            taints = getattr(instance, "taints", 0)
+            # All model instance inherited from SpliceDB also has "trusted" and "synthesized" columns.
+            trusted = getattr(instance, "trusted", True)
+            synthesized = getattr(instance, "synthesized", False)
+            # Convert from integer into bitarray (model instance's taints are integer)
+            tba = to_bitarray(taints)
+            return cls.splicify(val,
+                                trusted=trusted,
+                                synthesized=synthesized,
+                                taints=tba)
 
     def __set__(self, obj, value):
-        if isinstance(value, SpliceMixin):
-            obj.__dict__[self.field.name] = value
-            setattr(obj, self.synthesized_field_name, value.synthesized)
-            # Convert the bitarray into integer
-            taints = ba2int(value.taints, signed=True)
-            setattr(obj, self.taint_field_name, taints)
-        else:
-            obj.__dict__[self.field.name] = self.field.to_python(value)
+        obj.__dict__[self.field.name] = self.field.to_python(value)
 
 
 class SpliceFieldMixin(object):
+    """
+    All non-relational model fields should inherit from this mixin
+    to add a descriptor to the field. This is done during the
+    contribute_to_class call when models are being setup.
+    """
     def contribute_to_class(self, cls, name, private_only=False):
-        synthesized_field_name = "{}_synthesized".format(name)
-        taint_field_name = "{}_taint".format(name)
-        if not cls._meta.abstract and not hasattr(cls, synthesized_field_name):
-            synthesized_field = models.BooleanField(default=False)
-            # We must manually update creation_counter. Reference:
-            # https://blog.elsdoerfer.name/2008/01/08/fuzzydates-or-one-django-model-field-multiple-database-columns/
-            synthesized_field.creation_counter = self.creation_counter
-            cls.add_to_class(synthesized_field_name, synthesized_field)
-
-            taint_field = models.BigIntegerField(default=0)
-            taint_field.creation_counter = self.creation_counter
-            cls.add_to_class(taint_field_name, taint_field)
         super().contribute_to_class(cls, name)
-        setattr(cls, name, SpliceCreator(self))
-
-
-class SpliceIntegerField(SpliceFieldMixin, models.IntegerField):
-    pass
-
-
-class SpliceCharField(SpliceFieldMixin, models.CharField):
-    pass
-
-
-if __name__ == "__main__":
-    pass
+        setattr(cls, name, SpliceDescriptor(self))
