80a9a633f1da31b48e4c5d5311f89af194e56119
Michael Xueyuan Han-Vanbastelaer <hanx@g.harvard.edu>
2021-06-09 10:23:27 -0400

    Add a new option to use builtin int as user taint. Modify identity.py for int-based taint implementation.

--- django/splice/identity.py
+++ django/splice/identity.py
@@ -1,13 +1,18 @@
-from bitarray import bitarray
-from bitarray.util import int2ba, ba2int
+from django.splice.settings import BITARRAY
 
-MAX_USERS = 64
+if BITARRAY:
+    from bitarray import bitarray
+    from bitarray.util import int2ba, ba2int
+
+MAX_USERS = 63
 
 
 def empty_taint():
-    taint = bitarray(MAX_USERS, endian='big')
-    taint.setall(False)
-    return taint
+    if BITARRAY:
+        taint = bitarray(MAX_USERS, endian='big')
+        taint.setall(False)
+        return taint
+    return 0
 
 
 class TaintSource(object):
@@ -28,19 +33,32 @@ def set_current_user_taint(taint):
 
 def set_taint_from_id(uid):
     pos = uid % TaintSource.MAX_USERS
-    taint = empty_taint()
-    taint[pos] = True
+    if BITARRAY:
+        taint = empty_taint()
+        taint[pos] = True
+    else:
+        taint = 1 << pos
     set_current_user_taint(taint)
+
+
+# For int taint only
+def get_taint_from_id(uid):
+    pos = uid % TaintSource.MAX_USERS
+    return 1 << pos
 
 
 def to_int(taint):
     """Convert a bitarray taint to its corresponding integer."""
-    return ba2int(taint, signed=True)
+    if BITARRAY:
+        return ba2int(taint, signed=True)
+    return taint
 
 
 def to_bitarray(taint):
     """Convert an integer taint to its corresponding bitarray."""
-    return int2ba(taint, length=TaintSource.MAX_USERS, endian='big', signed=True)
+    if BITARRAY:
+        return int2ba(taint, length=TaintSource.MAX_USERS, endian='big', signed=True)
+    return taint
 
 
 def union(taint_1, taint_2):
@@ -48,16 +66,20 @@ def union(taint_1, taint_2):
     Union two taints and return a union-ed bit array.
     The two taints can either be an integer or a bit array.
     """
-    if not isinstance(taint_1, bitarray):
-        taint_1 = to_bitarray(taint_1)
-    if not isinstance(taint_2, bitarray):
-        taint_2 = to_bitarray(taint_2)
+    if BITARRAY:
+        if not isinstance(taint_1, bitarray):
+            taint_1 = to_bitarray(taint_1)
+        if not isinstance(taint_2, bitarray):
+            taint_2 = to_bitarray(taint_2)
+        return taint_1 | taint_2
     return taint_1 | taint_2
 
 
 def union_to_int(taint_1, taint_2):
     """Similar to union but return a union-ed integer."""
-    return to_int(union(taint_1, taint_2))
+    if BITARRAY:
+        return to_int(union(taint_1, taint_2))
+    return union(taint_1, taint_2)
 
 
 if __name__ == "__main__":
